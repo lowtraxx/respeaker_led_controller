77557e37b246730fbefab04a7540d134059c8b1c
Oliver Pahl <oliver.pahl@posteo.de>
2019-07-24 19:01:06 +0200

    Indented and beautified the code using Google codestyle.

--- LedController_Test.cpp
+++ LedController_Test.cpp
@@ -3,7 +3,7 @@
 ** support, and with no warranty, express or implied, as to its usefulness for
 ** any purpose. You are free to modify it and use it in any way you want,
 ** but you have to leave this header intact.
-**               
+**
 **
 ** LedController_Test.cpp
 ** A simple test app, that lights 4 LEDs of the ReSpeaker 4mic_hat using the
@@ -12,39 +12,38 @@
 ** Author: Oliver Pahl
 ** -------------------------------------------------------------------------*/
 #include "LedController.h"
-#include <new>
 #include <unistd.h>
 #include <cstdlib>
+#include <new>
 
-int main()
-{
-	// Initialize the Controller
-	// with 12 LEDs
-	LedController *led_controller = &LedController::GetInstance();
-	led_controller->PowerUp(12);
-	
-	// Clear the LEDs
-	led_controller->Clear();
-	
-	// Set a few pixel to test
-	// Red
-	led_controller->SetPixelColor(0, 24, 0, 0, 31);
-	
-	// Green
-	led_controller->SetPixelColor(3, 0, 24, 0, 31);
-	
-	// Blue
-	led_controller->SetPixelColor(6, 0, 0, 24, 31);
-	
-	// White
-	led_controller->SetPixelColor(9, 12, 12, 12, 31);
-	
-	// Show the pixels
-	led_controller->Show();
-	
-	// Sleep for a few seconds and the clean up
-	sleep(3);
-	led_controller->Clear();
-	
-	return 0;	
+int main() {
+  // Initialize the Controller
+  // with 12 LEDs
+  LedController *led_controller = &LedController::GetInstance();
+  led_controller->PowerUp(12);
+
+  // Clear the LEDs
+  led_controller->Clear();
+
+  // Set a few pixel to test
+  // Red
+  led_controller->SetPixelColor(0, 24, 0, 0, 31);
+
+  // Green
+  led_controller->SetPixelColor(3, 0, 24, 0, 31);
+
+  // Blue
+  led_controller->SetPixelColor(6, 0, 0, 24, 31);
+
+  // White
+  led_controller->SetPixelColor(9, 12, 12, 12, 31);
+
+  // Show the pixels
+  led_controller->Show();
+
+  // Sleep for a few seconds and the clean up
+  sleep(3);
+  led_controller->Clear();
+
+  return 0;
 }
